b1fe0c35325ced0e98ab8f0c944446bd0e11d326
Manoj <msbabu@asu.edu>
2016-11-07 22:09:04 -0700

    Have two way communication between client and service set up.
    Added comments

--- artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/ArtLib.java
+++ artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/ArtLib.java
@@ -6,15 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.MemoryFile;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
+import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 /**
  * Created by rlikamwa on 10/2/2016.
@@ -23,74 +30,133 @@ import java.io.IOException;
 public class ArtLib {
     private TransformHandler artlistener;
     private Activity myActivity;
-    public ArtLib(Activity activity){
+
+    public ArtLib(Activity activity) {
         myActivity = activity;
         initialize();
     }
+
+    // Messenger to get replies on from the Service.
+    private Messenger ServiceMessenger ;
+    // Messenger to send messages to Service
     private Messenger myMessenger;
     private boolean Bound;
-    // Handler to start a connection and close a connection between library and service.
-    ServiceConnection mServiceConnection = new ServiceConnection(){
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder service){
-        myMessenger = new Messenger(service);
-        Bound = true;
-    }
 
-    @Override
-    public void onServiceDisconnected(ComponentName name){
-        myMessenger = null;
-        Bound = false;
-    }
+
+    // Handler to start a connection and close a connection between library and service.
+    ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myMessenger = new Messenger(service);
+            ServiceMessenger = new Messenger(new TransformHandlerLib());
+            Bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            myMessenger = null;
+            ServiceMessenger = null;
+            Bound = false;
+        }
     };
 
     // Initialization Method. Binds the activity to the transform service.
-    public void initialize(){
+    public void initialize() {
         myActivity.bindService(new Intent(myActivity, TransformService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
     }
+
     // This array contains the list of transforms that the library supports. Also used to populate the drop down on the UI.
-    public String[] getTransformsArray(){
+    public String[] getTransformsArray() {
         String[] transforms = {"Gaussian Blur", "Neon edges", "Color Filter"};
         return transforms;
     }
 
     //
-    public TransformTest[] getTestsArray(){
+    public TransformTest[] getTestsArray() {
         TransformTest[] transforms = new TransformTest[3];
-        transforms[0]=new TransformTest(0, new int[]{1,2,3}, new float[]{0.1f, 0.2f, 0.3f});
-        transforms[1]=new TransformTest(1, new int[]{11,22,33}, new float[]{0.3f, 0.2f, 0.3f});
-        transforms[2]=new TransformTest(2, new int[]{51,42,33}, new float[]{0.5f, 0.6f, 0.3f});
+        transforms[0] = new TransformTest(0, new int[]{1, 2, 3}, new float[]{0.1f, 0.2f, 0.3f});
+        transforms[1] = new TransformTest(1, new int[]{11, 22, 33}, new float[]{0.3f, 0.2f, 0.3f});
+        transforms[2] = new TransformTest(2, new int[]{51, 42, 33}, new float[]{0.5f, 0.6f, 0.3f});
 
         return transforms;
     }
 
-    public void registerHandler(TransformHandler artlistener){
-        this.artlistener=artlistener;
+    public void registerHandler(TransformHandler artlistener) {
+        this.artlistener = artlistener;
     }
 
     // Request transform sends a request to the service to perform an image transform. It sends the relevant data to the Service.
-    // It is a non blocking, so it returns true if the request was successfully made. A new request can be placed before the old one has completed.
+    // It is non blocking, so it returns true if the request was successfully made. A new request can be placed before the old one has completed.
     public boolean requestTransform(Bitmap img, int index, int[] intArgs, float[] floatArgs) {
         try {
-            MemoryFile memFile = new MemoryFile("memKey", 30);
+            // Convert the bitmap image to byteArray
+            ByteArrayOutputStream bAos = new ByteArrayOutputStream();
+            img.compress(Bitmap.CompressFormat.PNG, 0, bAos);
+            byte[] bA = bAos.toByteArray();
+            // Create a memoryFile of size of the input  or now the byteArray.
+            MemoryFile memFile = new MemoryFile("imgKey", bA.length);
+            memFile.writeBytes(bA, 0, 0, bA.length);
+            String input = Arrays.toString(bA);
+            Log.d("Library Input", input);
             ParcelFileDescriptor pfd = MemoryFileUtil.getParcelFileDescriptor(memFile);
 
-            int what = TransformService.Transform_ONE;
-//            int what = TransformService.Transform_TWO;
+//            int what = index;
+            int what = TransformService.Transform_TWO;
 
-            // Sequence to send some dummy data to service. Will be replaced with sending actual data.
+            // Making data parcelable to be sent across to service
             Bundle dataBundle = new Bundle();
             dataBundle.putParcelable("pfd", pfd);
-            Message msg = Message.obtain(null, what, 2, 3);
+            // Build the message to send to Service.
+            Message msg = Message.obtain(null, what);
+            // Payload of the message
             msg.setData(dataBundle);
+            // Tell Service to reply on this Messenger.
+            msg.replyTo = ServiceMessenger;
             try {
-                myMessenger.send(msg);
+                myMessenger.send(msg);          // Send message to service.
+                memFile.close();                // Close to memory File before finalize().
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return true;
+
+        return true;                            // Return to user to allow new requests.
+    }
+
+    // Handler to receive messages from the Service. This is owned by the messenger dedicated for receive.
+    class TransformHandlerLib extends Handler {
+        @Override
+        public void handleMessage(Message mesg) {
+            // Unpack the data. Similar to what we do on the Service side when Library sent it a message.
+            Log.d("Library Response", "handleMessage(msg)" + mesg.what);
+            Bundle dataBundle = mesg.getData();
+            ParcelFileDescriptor pfd = (ParcelFileDescriptor) dataBundle.get("Outpfd");
+            //Convert the data bundle obtained into a byte array.
+            InputStream is = new FileInputStream(pfd.getFileDescriptor());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bytesRead;
+            byte[] out = new byte[16384];
+
+            try {
+                while ((bytesRead = is.read(out, 0, out.length)) != -1) {
+                    baos.write(out, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                baos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Decode the byteArray back into a bitmap to be displayed.
+            Bitmap bitmap = BitmapFactory.decodeByteArray(baos.toByteArray(), 0, baos.size());
+            // Convert the byteArray to a string for easy viewability.
+            String temp = Arrays.toString(baos.toByteArray());
+            Log.d("LibraryResponse", temp);
+        }
     }
 }
--- artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/TransformService.java
+++ artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/TransformService.java
@@ -1,17 +1,27 @@
 package edu.asu.msrs.artcelerationlibrary;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.MemoryFile;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
+import android.os.RemoteException;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class TransformService extends Service {
     public TransformService() {
@@ -19,23 +29,80 @@ public class TransformService extends Service {
     String TAG = "Service_TAG";
     static final int Transform_ONE = 1;
     static final int Transform_TWO = 2;
+    // Messenger to receive the message from the Library.
+    final Messenger ServiceMessenger = new Messenger (new TransformHandler());
+    // Messenger to send the message to the Library.
+    private Messenger ClientMessenger ;
+
     // Handler to choose the transform to apply based on value sent from library.
     // This can spawn off threads to do the actual work.
     class TransformHandler extends Handler {
         @Override
         public void handleMessage(Message mesg) {
             Log.d(TAG, "handleMessage(msg)" + mesg.what);
-            // Using a Switch-case to select the right transform
+            // Using a Switch-case to select the right transform or action to perform. For now hardcoded.
             switch(mesg.what){
                 case Transform_ONE:
                     Log.d(TAG, "Transform_ONE");
                     break;
                 case Transform_TWO:
+                    // Obtain the messenger handler from the message.
+                    ClientMessenger = mesg.replyTo;
+                    // Get the data bundle as received from the service.
                     Bundle dataBundle = mesg.getData();
                     ParcelFileDescriptor pfd = (ParcelFileDescriptor)dataBundle.get("pfd");
-                    FileInputStream fios = new FileInputStream(pfd.getFileDescriptor());
-                    int result = mesg.arg1 * mesg.arg2;
-                    Log.d(TAG, "Transform_TWO" + result);
+                    // Convert the bundle into a byteArray.
+                    InputStream is = new FileInputStream(pfd.getFileDescriptor());
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int bytesRead;
+                    // ByteArray size hardcoded for now. Will make it more dynamic.
+                    byte[] out = new byte[16384];
+                    try {
+                        while((bytesRead = is.read(out,0,out.length)) != -1){
+                        baos.write(out,0,bytesRead);
+                    }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        baos.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(baos.toByteArray(), 0, baos.size());
+                    // Convert the outputStream to String for easy printing/viewing on log.
+                    String temp = Arrays.toString(baos.toByteArray());
+                    Log.d("Service Output",temp);
+                    // Converting to byteArray.
+                    ByteArrayOutputStream outbAos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG,0,outbAos);
+                    byte[] outbA = outbAos.toByteArray();
+                    // Do a garbage transformation. Here the function decrements each element by 1.
+                    outbA = transformImg(outbA);
+                    // Print this "Transformed" byteArray.
+                    String TfrbA = Arrays.toString(outbA);
+                    Log.d("Service Response",TfrbA);
+
+                    // Prepare a memoryFile to send the message back to the Library.
+                    MemoryFile memFile = null;
+                    Bundle outdataBundle = new Bundle();
+                    ParcelFileDescriptor Outpfd;
+                    try {
+                        memFile = new MemoryFile("imgOutKey", outbA.length);
+                        memFile.writeBytes(outbA,0,0,outbA.length);
+                        Outpfd = MemoryFileUtil.getParcelFileDescriptor(memFile);
+                        outdataBundle.putParcelable("Outpfd", Outpfd);
+                        Message outmsg = Message.obtain(null, 1);
+                        outmsg.setData(outdataBundle);
+                        // Remember to use the messenger created just for send.
+                        ClientMessenger.send(outmsg);
+                        memFile.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     Log.d(TAG, "Default");
@@ -44,9 +111,46 @@ public class TransformService extends Service {
         }
     }
 
-    final Messenger mMessenger = new Messenger (new TransformHandler());
+    // A simple function that "transforms" the input image.
+    public byte[] transformImg(byte[] In){
+        for( int i=0; i < In.length;i++){
+            In[i]--;
+        }
+        return In;
+    }
+
+
     @Override
     public IBinder onBind(Intent intent){
-        return mMessenger.getBinder();
+        return ServiceMessenger.getBinder();
     }
+
+//    class ThreadTask extends Thread{
+//        int my_ticket;
+//        ThreadTask(int a){
+//            this.my_ticket = a;
+//        }
+//        @Override
+//        public void run() {
+//                synchronized(barrier) {
+                      //while(current_ticket)
+//                    Log.d("TTask", "hey before " + a);
+//                    try {
+//                        Thread.sleep(a * 1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                Log.d("TTask", "hey after " + a);
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
 }
+
+
+
